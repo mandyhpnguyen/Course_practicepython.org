d81b7b1f24c1a6a9d895a3654b304b037218c497
mandyhpnguyen <mandyhpnguyen@gmail.com>
2022-06-12 18:17:24 -0500

    Update

--- test.py
+++ test.py
@@ -1,43 +1,14 @@
-count = 0
-
-while True:
-
+def generate_password():
     import random
-    import sys
-
-    randomNum = random.randint(1, 9)
-    guessNum = input("\nEnter a Number (0-9) to Guess: ")
-
-    if guessNum == "exit" or guessNum == "e":
-        print("\n> End of Program!! <\n")
-        sys.exit()
-        break
-    elif type(int(guessNum)) == int:
-        
-        guessNum = int(guessNum)
-        
-        if randomNum == guessNum:
-            print(f"You guessed exactly! We both guessed {randomNum}!!")
-        elif randomNum < guessNum:
-            print(f"Your number {guessNum} is larger than our number {randomNum} by the distance of {guessNum-randomNum}")
-        else:
-            print(f"Your number {guessNum} is smaller than our number {randomNum} by the distance of {-guessNum+randomNum}")
-
-        count = count + 1
-        print(f"Total Play Time(s): {count}")
-        
-        while True:
-            exit_prompt = input("\nWould you like to play again? (Yes/No): ").lower()
-            yesPromptList = ["y", "yes"]
-            noPromptList = ["n", "no"]
-            if exit_prompt in yesPromptList:
-                break
-            elif exit_prompt in noPromptList:
-                print("\n> End of Program!! <\n")
-                sys.exit()
-                break
-            else:
-                print("Error: Try Again!!")
-                continue
-    else:
-        print("Error: Try Again!!")
+    
+    charaters = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    charatersList = [character for character in charaters]
+    
+    characterCount = int(input("Enter the Number of Characters in Your Passwords: "))
+    
+    passwordList = []
+    for i in range(characterCount):
+        passwordList.append(random.choice(charatersList))
+    
+    password = "".join(passwordList)
+    print(password)
